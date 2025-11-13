05e8e779b767f35b357f6ac7009ed57c6c704446
Nick23Watson <94018364+Nick23Watson@users.noreply.github.com>
2025-11-12 22:28:58 -0500

    Preliminary code for ASOS analysis
    Still figuring out how to parse through data and extract necessary data. Might need to request more because not a lot of RH data was collected from these sites.

---
+++ Scripts/Texas/ASOS Observational Analysis/TX_ASOS_Analysis.m
@@ -0,0 +1,5 @@
+%% Nicholas Watson - Energy Meteorology Final Project
+% Icing on Turbines - ERA5 Reanalysis vs Observational Data
+% Version 1.0 - 11/12/25
+% Texas - February 2021 (Winter Storm Uri)
+% ASOS Observational Analysis
